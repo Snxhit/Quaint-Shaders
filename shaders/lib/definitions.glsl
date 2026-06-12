71014fa2ee5fc2bbe8ae6e27c0f1422a71c380ab
Snxhit <amitdgames@gmail.com>
2026-06-12 08:52:56 +0530

    add exclusion toggle to settings, translation, change default toggle value

--- shaders/lib/definitions.glsl
+++ shaders/lib/definitions.glsl
@@ -18,7 +18,7 @@
 #define BLOOM_THRESHOLD 0.9 // [0.3 0.5 0.7 0.9]
 
 // composite6.fsh
-#define CHROMATIC_ABERRATION 1 // [0 1]
+#define CHROMATIC_ABERRATION 0 // [0 1]
 #define CHROMATIC_ABERRATION_STRENGTH 0.05 // [0.01 0.025 0.05 0.1]
 
 // composite7.fsh
