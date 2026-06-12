8918a03271120d7c1a595886dbe2e620621a9928
Snxhit <amitdgames@gmail.com>
2026-06-12 08:54:41 +0530

    fix default toggle value

--- shaders/lib/definitions.glsl
+++ shaders/lib/definitions.glsl
@@ -11,14 +11,14 @@
 #define EDGE_DETECTION 1 // [0 1]
 #define EDGE_DETECTION_STRENGTH 0.6 // [0.3 0.4 0.5 0.6 0.7]
 #define EDGE_SIZE 2.0 // [1.0 2.0 3.0]
-#define EXCLUDE_FOLIAGE 0 // [0 1]
+#define EXCLUDE_FOLIAGE 1 // [0 1]
 
 // composite3.fsh
 #define BLOOM_ON 1 // [0 1]
 #define BLOOM_THRESHOLD 0.9 // [0.3 0.5 0.7 0.9]
 
 // composite6.fsh
-#define CHROMATIC_ABERRATION 0 // [0 1]
+#define CHROMATIC_ABERRATION 1 // [0 1]
 #define CHROMATIC_ABERRATION_STRENGTH 0.05 // [0.01 0.025 0.05 0.1]
 
 // composite7.fsh
