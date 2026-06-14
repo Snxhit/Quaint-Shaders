a97b6ceca8450e5ef71ca0f68ae1e08be6ae2d6f
Snxhit <amitdgames@gmail.com>
2026-06-14 20:18:39 +0530

    add FXAA composite 8 pass - also rewrite defns comments

--- shaders/lib/definitions.glsl
+++ shaders/lib/definitions.glsl
@@ -1,27 +1,40 @@
-// composite.fsh
+// first composite pass
+// custom lighting & shadows
 #define CUSTOM_LIGHTING 1 // [0 1]
 #define SHADOW_RADIUS 2 // [1 2 3 4]
 #define SHADOW_RANGE 4 // [3 4 5]
 
-// composite1.fsh
+// second composite pass
+// fog
 #define FOG_ON 1 // [0 1]
 #define FOG_DENSITY 5.0 // [5.0]
 
-// composite2.fsh
+// third composite pass
+// edge detection
 #define EDGE_DETECTION 1 // [0 1]
 #define EDGE_DETECTION_STRENGTH 0.6 // [0.3 0.4 0.5 0.6 0.7]
 #define EDGE_SIZE 2.0 // [1.0 2.0 3.0]
 #define EDGE_BRIGHTNESS 3.5 // [1.5 2.0 2.5 3.0 3.5 4.0]
 #define EXCLUDE_FOLIAGE 1 // [0 1]
 
-// composite3.fsh
+// fourth composite pass
+// bloom (applies to the whole screen)
 #define BLOOM_ON 1 // [0 1]
 #define BLOOM_THRESHOLD 0.9 // [0.3 0.5 0.7 0.9]
 
-// composite6.fsh
+// fifth composite pass
+// chromatic aberration
 #define CHROMATIC_ABERRATION 1 // [0 1]
 #define CHROMATIC_ABERRATION_STRENGTH 0.05 // [0.01 0.025 0.05 0.1]
 
-// composite7.fsh
+// sixth composite pass
+// ACES tone mapping
 #define COLOR_MAPPING 1 // [0 1]
+
+// seventh composite pass
+// FXAA
 #define FXAA_ENABLED 1 // [0 1]
+#define FXAA_MIN_THRESHOLD 0.0312
+#define FXAA_MAX_THRESHOLD 0.125
+#define FXAA_ITERATIONS 8
+#define SUBPIXEL_QUALITY 0.75
